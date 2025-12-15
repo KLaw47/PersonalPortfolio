fe9fa754213375adb202944d8374aaf38cbd9714
Keaton Law <keaton.c.law@gmail.com>
2025-12-14 21:05:50 -0600

    add viewsents and serializers for blogpost and project

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import BlogPost, Project
+from .serializers import BlogPostSerializer, ProjectSerializer
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True #if the request from the user is a get, we allow this, its a "safe method"
+        return request.user and request.user.is_staff #If the request is not one of those safe methods, 
+    #we are first checking that the user exists, and then the value of is_staff
+
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer #todo, make serializers
+    permission_classes = [IsAdminOrReadOnly]
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAdminOrReadOnly]
